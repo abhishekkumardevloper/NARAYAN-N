b2b380c8114c24cfccd6054e7e73a49a79f0be95
abhishekkumardevloper <abhishekkumar.devloper@gmail.com>
2026-01-02 00:05:59 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -48,7 +48,7 @@ const Home = () => {
           {/* LOGO */}
           <div className="flex items-center space-x-3 cursor-pointer">
             <img
-              src="n1.png"
+              src="/n1.png"
               alt="NarayanGrowth Logo"
               className="w-10 h-10 object-contain"
             />
